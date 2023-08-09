e144d592c8dda36ee3ba02ecb2e235265defdf17
yezy <iyeahji@gmail.com>
2023-08-09 20:52:52 +0900

    feat: Boj_11720_숫자의_합

---
+++ yezy/boj/src/main/java/org/example/Boj_11720_숫자의_합.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Boj_11720_숫자의_합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
